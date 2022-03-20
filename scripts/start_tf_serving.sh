3ed551332697d93f1f2e31fc3ccf9d7662d948f3
dimitreOliveira <dimitreandrew@gmail.com>
2022-03-20 18:28:34 -0300

    Updating tf serving script

--- scripts/start_tf_serving.sh
+++ scripts/start_tf_serving.sh
@@ -1,14 +1,8 @@
-# torchserve --start \
-# --ncs \
-# --model-store model-store \
-# --models fastrcnn=fastrcnn.mar
-
-# docker run -p 8501:8501 \
-#   --mount type=bind,source=$MODEL_SOURCE,target=$MODEL_TARGET \
-#   -e MODEL_NAME=$MODEL_NAME -t tensorflow/serving
-
-
+MODEL_SOURCE=$(pwd)/models/multi-label/1
+MODEL_TARGET=/models/multi-label/1
+MODEL_NAME=multi-label
 
 docker run -p 8501:8501 \
-  --mount type=bind,source=$(pwd)/models/multi-label/1,target=/models/multi-label/1 \
-  -e MODEL_NAME=multi-label -t tensorflow/serving
+  --mount type=bind,source=$MODEL_SOURCE,target=$MODEL_TARGET \
+  -e MODEL_NAME=$MODEL_NAME \
+  -t tensorflow/serving
